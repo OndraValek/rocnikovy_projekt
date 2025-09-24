933305e3ffa953ee4ad43e1a912e42d4c3a6c5f1
OndraValek <120708124+OndraValek@users.noreply.github.com>
2025-09-24 16:47:10 +0200

    Add files via upload

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*ebmvwfuf&lb)jg567%^1(mh$rm1d0-(iiw)3v4+nkh16a%k6="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
