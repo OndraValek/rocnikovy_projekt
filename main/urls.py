fbaeac89b0a4357b33008310ad4c2329c6598808
OndraValek <120708124+OndraValek@users.noreply.github.com>
2025-11-14 22:02:41 +0100

    Delete main directory

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('topic/<int:topic_id>/', views.topic_detail, name='topic_detail'),
-]
-
